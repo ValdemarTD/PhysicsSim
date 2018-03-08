214d6d46920dc63430fa12daad94b69cdef012bf
ValdemarTD <zacharyianlee@gmail.com>
2018-03-08 06:03:36 -0800

    Added exampleModule.py to explain module format

---
+++ exampleModule.py
@@ -0,0 +1,17 @@
+from visual import *
+
+bodies = []
+forces = []
+joints = []
+
+
+
+def Your_New_Module(bodies, forces, joints):
+    #Your
+    #code
+    #goes
+    #here
+    return(forces)
+
+
+modules = [Your_New_Module]
